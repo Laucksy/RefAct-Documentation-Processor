03ce517a11d289ee596432581a66cbaa0828fe9e
Erik Laucks <phisigmapi314@gmail.com>
2018-10-24 22:03:59 -0400

    Adding sections to chapters

--- db/chapter.js
+++ db/chapter.js
@@ -5,7 +5,11 @@ const chapterSchema = new mongoose.Schema({
   _id: {type: Types.ObjectId, default: new mongoose.Types.ObjectId()},
   number: {type: Number, required: true, unique: true},
   title: {type: String, required: true, unique: true},
-  intro: {type: String, default: ''}
+  intro: {type: String, default: ''},
+  sections: [{
+    title: {type: String, default: ''},
+    description: {type: String, default: ''}
+  }]
 })
 
 const Chapter = mongoose.model('Chapter', chapterSchema)
--- helpers/file.js
+++ helpers/file.js
@@ -11,7 +11,11 @@ export const writeToFile = (file, chapters) => {
 
   chapters.sort((a, b) => a.number - b.number).forEach(chapter => {
     result += `\\chapter{${chapter.title}}\n`
-    result += chapter.intro.replace(/\t/g, '\\tab').replace(/\n/g, '\\\\\n')
+    result += formatText(chapter.intro)
+    chapter.sections.forEach(section => {
+      result += `\\section{${section.title}}\n`
+      result += formatText(section.description)
+    })
     result += '\n'
   })
 
@@ -26,4 +30,26 @@ export const readFromFile = (inputFile) => {
 
 export const generateOutputStream = (outputFile) => {
   return fs.createWriteStream(`public/${outputFile}`)
+}
+
+export const formatText = (str) => {
+  let output = str.replace(/\t/g, '\\tab').replace(/\n/g, '\\\\\n')
+
+  if (output.indexOf('LIST[[') >= 0) {
+    let index = output.indexOf('LIST[[')
+    let front = output.substring(0, index)
+    let back = output.substring(output.indexOf(']]', index) + 1)
+
+    index += 5
+    let list = '\\begin{enumerate}\n\\itemsep0em\n\\setlength{\\itemindent}{2em}\n'
+    while (output.indexOf('[', index) > 0) {
+      list = list + '\\item ' + output.substring(output.indexOf('[', index) + 1, output.indexOf(']', index)) + '\n'
+      index = output.indexOf(']', index) + 1
+    }
+    list = list + '\\end{enumerate}\n'
+
+    output = front + list + back
+  }
+
+  return output
 }
